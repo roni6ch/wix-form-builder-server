c858f9a5557507d68988f27a50add90ccc33f56f
Roni <roni691986@gmail.com>
2020-06-16 16:37:53 +0300

    added procfile for heroku

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './forms.module';
 
 async function bootstrap() {
   const port = 3000;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors();
   await app.listen(process.env.PORT || port);
   console.clear();
